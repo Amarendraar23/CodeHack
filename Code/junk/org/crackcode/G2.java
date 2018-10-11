c18e070a55373838cc0c471e2916ecc261635acb
amareddy <amarendra.reddy@nokia.com>
2018-10-11 21:23:54 +0530

    Count Inversions in an array(Using Merge Sort)

--- Code/junk/org/crackcode/G2.java
+++ Code/junk/org/crackcode/G2.java
@@ -1,5 +1,9 @@
 package org.crackcode;
 
+//Inversion Count for an array indicates – how far (or close) the array is from being sorted. 
+//If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum. 
+//Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j 
+
 public class G2 {
 
 	public static void main(String args[]) {
