a43b332dc960ed32db123394e3d472c3a769468b
amareddy <amarendra.reddy@nokia.com>
2018-11-01 15:14:38 +0530

    Updated G5

--- Code/junk/org/crackcode/arrays/G5.java
+++ Code/junk/org/crackcode/arrays/G5.java
@@ -11,9 +11,9 @@ public class G5 {
 
 	public static void main(String[] args) 
     { 
-		int arr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3}; 
+		int arr[] = {1,3}; 
         int n = arr.length; 
-        int key = 6; 
+        int key = 0; 
         int i = search(arr, 0, n-1, key); 
         if (i != -1)  
             System.out.println("Index: " + i); 
@@ -22,16 +22,18 @@ public class G5 {
     }
 
 	private static int search(int[] arr, int i, int j, int key) {
+		if(arr.length==0)
+			return -1;
 		int pivot = findPivot(arr);
 		int index = binarySearch(arr,key,0,pivot);
 		if(index!=-1)
 			return index;
 		else
-			return binarySearch(arr,key,pivot+1,arr.length);
+			return binarySearch(arr,key,pivot+1,arr.length-1);
 	}
 
 	private static int binarySearch(int[] arr, int key, int start, int end) {
-		while(start<end) {
+		while(start<=end) {
 			int mid = (start+end)/2;
 			if(arr[mid] == key)
 				return mid;
@@ -45,17 +47,17 @@ public class G5 {
 
 	private static int findPivot(int[] arr) {
 		int low = 0;
-		int high = arr.length;
+		int high = arr.length-1;
 		while(low<high) {
 			int mid = (high+low)/2;
 			if(mid>low && arr[mid]<arr[mid-1])
 				return mid-1;
 			else if(mid<high && arr[mid]>arr[mid+1])
 				return mid;
-			else if(mid>low && arr[mid]>arr[mid-1])
-				low = mid;
+			else if(arr[low]>=arr[mid])
+				high = mid-1;
 			else
-				high = mid;
+				low = mid+1;
 		}
 		return 0;
 	}
